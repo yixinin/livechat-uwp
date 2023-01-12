237d2f604c2c48695394e0d8c89d7ac75027da47
eason <yixinin@outlook.com>
2023-01-12 08:52:15 +0800

    - init

---
+++ Livechat UWP/MainPage.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.Devices;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace Livechat_UWP
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaCapture captureManager = null;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        async private void PhotographButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (captureManager == null)
+            {
+                capturePreview.Visibility = Visibility.Visible;
+                ProfilePic.Visibility = Visibility.Collapsed;
+                captureManager = new MediaCapture();
+
+                //选择后置摄像头
+                DeviceInformation cameraDevice = await FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel.Back);
+                if (cameraDevice == null)
+                {
+                    System.Diagnostics.Debug.WriteLine("No camera device found!");
+                    return;
+                }
+                var settings = new MediaCaptureInitializationSettings
+                {
+                    StreamingCaptureMode = StreamingCaptureMode.Video,
+                    //MediaCategory = MediaCategory.Other,
+                    //AudioProcessing = AudioProcessing.Default,
+                    PhotoCaptureSource = PhotoCaptureSource.Auto,
+                    AudioDeviceId = string.Empty,
+                    VideoDeviceId = cameraDevice.Id
+                };
+                await captureManager.InitializeAsync(settings);
+
+                using (var s = new ProducerConsumerStream())
+                {
+
+                    //var stream = s.AsRandomAccessStream();
+                    var encodingProfile = MediaEncodingProfile.CreateHevc(VideoEncodingQuality.HD1080p);
+                    await captureManager.StartRecordToStreamAsync(encodingProfile, s);
+                }
+
+            }
+        }
+
+
+        private static async Task<DeviceInformation> FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel desired)
+        {
+
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            DeviceInformation desiredDevice = allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null && x.EnclosureLocation.Panel == desired);
+
+            return desiredDevice ?? allVideoDevices.FirstOrDefault();
+        }
+        //拍照
+        async private void CapturePhoto_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (captureManager != null)
+            {
+                capturePreview.Visibility = Visibility.Collapsed;
+                ProfilePic.Visibility = Visibility.Visible;
+                ProfilePic.Source = null;
+                //declare string for filename
+                string captureFileName = string.Empty;
+
+                //图片格式
+                ImageEncodingProperties format = ImageEncodingProperties.CreateJpeg();
+
+                //创建本地存储文件夹
+                StorageFile file = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                    "Photo" + DateTime.Now.ToString("yyMMddHHmmss") + ".jpg",
+                    CreationCollisionOption.ReplaceExisting);
+
+                await captureManager.CapturePhotoToStorageFileAsync(format, file);
+
+                BitmapImage bmpImage = new BitmapImage(new Uri(file.Path));
+
+                ProfilePic.Source = bmpImage;//释放摄像头资源
+                capturePreview.Visibility = Visibility.Visible;
+                ProfilePic.Visibility = Visibility.Collapsed;
+                //captureManager.Dispose();
+                //captureManager = null;
+            }
+
+        }
+        private void FocusValueSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            try
+            {
+
+                uint focus = Convert.ToUInt32(e.NewValue);
+                SetFocus(focus);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+        //设置摄像头焦点方法
+        private async void SetFocus(uint? focusValue = null)
+        {
+
+            if (captureManager == null)
+            {
+                return;
+            }
+
+            try
+            {
+
+                if (!focusValue.HasValue)
+                {
+                    focusValue = 50;
+                }
+
+                if (captureManager.VideoDeviceController.FocusControl.Supported)
+                {
+
+                    captureManager.VideoDeviceController.FlashControl.AssistantLightEnabled = false;
+
+                    captureManager.VideoDeviceController.FocusControl.Configure(new FocusSettings() { Mode = FocusMode.Manual, Value = focusValue, DisableDriverFallback = true });
+
+                    await captureManager.VideoDeviceController.FocusControl.FocusAsync();
+                }
+            }
+            catch { }
+        }
+
+        async private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            await captureManager.StopRecordAsync();
+        }
+    }
+}
---
+++ Livechat UWP/ProducerConsumerStream.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+
+namespace Livechat_UWP
+{
+    class ProducerConsumerStream : IRandomAccessStream
+    {
+        private readonly ClientWebSocket ws;
+
+        private byte[] data;
+
+        private ulong position;
+
+        public ProducerConsumerStream()
+        {
+            ws = new ClientWebSocket();
+            var webSocketUrl = "ws://127.0.0.1:9002/live";
+            ws.ConnectAsync(new Uri(webSocketUrl), CancellationToken.None).Wait();
+            data = new byte[4096];
+        }
+
+        public bool CanRead { get { return false; } }
+
+        public bool CanSeek { get { return true; } }
+
+        public bool CanWrite { get { return true; } }
+
+        public void Flush()
+        {
+            if (position > 0)
+            {
+                var buf = new byte[position];
+                Array.Copy(data, buf, (int)position);
+                this.ws.SendAsync(buf, WebSocketMessageType.Binary, false, CancellationToken.None).Wait();
+            }
+            position = 0;
+        }
+
+        public long Length
+        {
+            get
+            {
+                return data.Length;
+            }
+        }
+
+        public ulong Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            return 0;
+        }
+
+        public ulong Seek(ulong offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    position = offset;
+                    break;
+                case SeekOrigin.Current:
+                    position += offset;
+                    break;
+                case SeekOrigin.End:
+                    position -= offset;
+                    break;
+            }
+            return position;
+        }
+        public void Seek(ulong offset)
+        {
+            this.Seek(offset, SeekOrigin.Begin);
+        }
+
+        public void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+            if (offset > 0)
+            {
+                position += (ulong)offset;
+            }
+
+            var n = 0;
+
+            while (n < count)
+            {
+                for (var i = 0; i < (int)position + count - n; i++)
+                {
+                    data[i] = buffer[n];
+                    n++;
+
+                    if (i == data.Length - 1)
+                    {
+                        position = 0;
+                        this.ws.SendAsync(data, WebSocketMessageType.Binary, false, CancellationToken.None).Wait();
+                        break;
+                    }
+                }
+            }
+            if (position > 0)
+            {
+                var buf = new byte[position];
+                Array.Copy(data, buf, (int)position);
+                this.ws.SendAsync(buf, WebSocketMessageType.Binary, false, CancellationToken.None).Wait();
+            }
+            position = 0;
+        }
+
+        public IInputStream GetInputStreamAt(ulong position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOutputStream GetOutputStreamAt(ulong position)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IRandomAccessStream CloneStream()
+        {
+            throw new NotImplementedException();
+        }
+
+        ulong IRandomAccessStream.Position => this.Position;
+
+
+
+
+        public IAsyncOperationWithProgress<IBuffer, uint> ReadAsync(IBuffer buffer, uint count, InputStreamOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncOperationWithProgress<uint, uint> WriteAsync(IBuffer buffer)
+        {
+            var n = 0;
+            var result = new AsyncOperationWithProgress<uint>(() =>
+            {
+                while (n < buffer.Length)
+                {
+                    for (var i = 0; i < (int)position + buffer.Length - n; i++)
+                    {
+                        data[i] = buffer.ToArray()[n];
+                        n++;
+
+                        if (i == data.Length - 1)
+                        {
+                            position = 0;
+                            this.ws.SendAsync(data, WebSocketMessageType.Binary, false, CancellationToken.None).Wait();
+                            break;
+                        }
+                    }
+                }
+                if (position > 0)
+                {
+                    var buf = new byte[position];
+                    Array.Copy(data, buf, (int)position);
+                    this.ws.SendAsync(buf, WebSocketMessageType.Binary, false, CancellationToken.None).Wait();
+                }
+                position = 0;
+                return Task.Run(() =>
+                {
+                    return (uint)n;
+                });
+            });
+
+            return result;
+        }
+
+        public IAsyncOperation<bool> FlushAsync()
+        {
+            var result = new AsyncOperation<bool>(() =>
+            {
+                this.Flush();
+                return Task.Run(() =>
+                {
+                    return (uint)(0);
+                });
+            });
+            return result;
+
+        }
+
+        public void Dispose()
+        {
+            this.ws.Dispose();
+        }
+
+        public ulong Size { get => (ulong)data.Length; set => throw new NotImplementedException(); }
+    }
+
+    internal class AsyncOperationWithProgress<TResult> : IAsyncOperationWithProgress<TResult, uint>
+    {
+        #region Fields
+
+        private readonly Task<TResult> mWorker;
+
+        private AsyncStatus mStatus;
+
+        #endregion
+
+
+        #region IAsyncOperationWithProgress properties
+
+        /// <summary>
+        /// Callback to report operation progress.
+        /// </summary>
+        AsyncOperationProgressHandler<TResult, uint> IAsyncOperationWithProgress<TResult, uint>.Progress { get; set; }
+
+
+        /// <summary>
+        /// Callback to report operation completion.
+        /// </summary>
+        AsyncOperationWithProgressCompletedHandler<TResult, uint> IAsyncOperationWithProgress<TResult, uint>.Completed { get; set; }
+
+
+        Exception IAsyncInfo.ErrorCode => mWorker.Exception;
+
+
+        uint IAsyncInfo.Id => (uint)mWorker.Id;
+
+
+        AsyncStatus IAsyncInfo.Status => mStatus;
+
+        #endregion
+
+
+        #region Init and clean-up
+
+        public AsyncOperationWithProgress(Func<Task<TResult>> workerFn)
+        {
+            mWorker = workerFn.Invoke();
+            mWorker.ContinueWith(task =>
+            {
+                TResult res = default;
+
+                if (task.IsFaulted)
+                {
+                    mStatus = AsyncStatus.Error;
+                }
+                else
+                {
+                    mStatus = AsyncStatus.Completed;
+                    res = task.Result;
+                }
+
+                    (this as IAsyncOperationWithProgress<TResult, uint>).Completed?.Invoke(this, mStatus);
+                return res;
+            });
+        }
+
+        #endregion
+
+
+        #region IAsyncOperationWithProgress API
+
+        TResult IAsyncOperationWithProgress<TResult, uint>.GetResults()
+        {
+            if (mStatus != AsyncStatus.Completed)
+                throw new ArgumentException($"Cannot get result when status is {mStatus}.");
+
+            return mWorker.Result;
+        }
+
+
+        void IAsyncInfo.Cancel()
+        {
+            // Do nothing
+        }
+
+
+        void IAsyncInfo.Close()
+        {
+            // Do nothing
+        }
+
+        #endregion
+    }
+    internal class AsyncOperation<TResult> : IAsyncOperation<TResult>
+    {
+        private TResult mStatus;
+
+        public AsyncOperation(Func<Task<uint>> value)
+        {
+        }
+
+        public TResult GetResults()
+        {
+            return mStatus;
+        }
+
+        public AsyncOperationCompletedHandler<TResult> Completed { get; set; }
+
+        public void Cancel()
+        {
+        }
+
+        public void Close()
+        {
+
+        }
+
+        public Exception ErrorCode => null;
+
+        public uint Id => 0;
+
+        public AsyncStatus Status => AsyncStatus.Completed;
+    }
+}
